7b5f70d8568eb0779051b9d2684020c7adcc6306
Сергей Вилков <sergey.vilkov@rsv.ru>
2025-01-10 17:36:23 +0300

    Build optimization

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,17 @@ import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 export default defineConfig({
+  build: {
+      rollupOptions: {
+          output:{
+              manualChunks(id) {
+                  if (id.includes('node_modules')) {
+                      return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                  }
+              }
+          }
+      }
+  },
   plugins: [
     vue({
       template: { transformAssetUrls }
